b1e0c6368498bb792c42305734f9bd54ed2d57f7
sandernor <sandersala123@gmail.com>
2023-09-15 01:13:59 +0200

    meshrendering works

--- Assets/Scripts/TriangleGen.cs
+++ Assets/Scripts/TriangleGen.cs
@@ -14,11 +14,28 @@ public class TriangleGen : MonoBehaviour
 
     public TextAsset vertexData;
     private char[] vertexDataArray;
-    private List <string> line;
+    private List<string> line;
     private int lineCount = 0;
-    private List <Vertex> vertexArray;
+    private List<Vertex> vertexArray;
     private Vertex v;
+    private Mesh mesh;
+    private List<int> triangles;
+    private int[] triVerts;
 
+
+    private void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    private void MeshUpdate()
+    {
+        mesh.Clear();
+        Vector3[] vertPos = new Vector3[] { vertexArray[0].position, vertexArray[1].position, vertexArray[2].position, vertexArray[3].position, vertexArray[4].position, vertexArray[5].position };
+        mesh.vertices = vertPos;
+
+        mesh.triangles = triVerts;
+    }
 
     private void FileToLines()
     {
@@ -36,26 +53,58 @@ public class TriangleGen : MonoBehaviour
         for (int i = 0; i < lineCount; i++)
         {
             line.Add(file2.ReadLine());
-            Debug.Log("Linje " + i + " : " + line[i]);
+            //Debug.Log("Linje " + i + " : " + line[i]);
         }
     }
 
     private void GenerateVertices()
     {
        int numVertices = int.Parse(line[0]);
-       for (int i = 0; i < numVertices; i++) 
+       for (int i = 1; i < numVertices+1; i++) 
        {
-            v = new Vertex();
+            string[] splitLine = line[i].Split(char.Parse(" "));
 
+            float x, y, z;
+            x = float.Parse(splitLine[0]);
+            y = float.Parse(splitLine[1]);
+            z = float.Parse(splitLine[2]);
+
+            v = new Vertex();
+            v.position = new Vector3(x, y, z);
             vertexArray.Add(v);
-       }
+            //Debug.Log(v.position);
+
+        }
+    }
+
+    private void GenerateTriangles()
+    {
+        int numVertices = int.Parse(line[0]);
+        int numTriangles = int.Parse(line[numVertices + 1]);
+        //for (int i = numVertices + 2; i < numVertices + 2 + numTriangles; i+=1)
+        //{
+        //    triangles.Add(int.Parse(line[i]));
+        //    Debug.Log(numTriangles);
+        //}
+
+        triVerts = new int[] {int.Parse(line[numVertices + 2]), int.Parse(line[numVertices + 3]), int.Parse(line[numVertices + 4]), int.Parse(line[numVertices + 5]), int.Parse(line[numVertices + 6]), int.Parse(line[numVertices + 7]), int.Parse(line[numVertices + 8]), int.Parse(line[numVertices + 9]), int.Parse(line[numVertices + 10]), int.Parse(line[numVertices + 11]), int.Parse(line[numVertices + 12]), int.Parse(line[numVertices + 13])};
+
+        //for (int i = 0; i < lineCount; i++)
+        //{
+        //    //Debug.Log("Linje " + i + " : " + line[i]);
+        //}
+
     }
 
     private void Start()
     {
         line = new List<string> ();
         vertexArray = new List<Vertex>();
+        triangles = new List<int> ();
         FileToLines();
+        GenerateVertices();
+        GenerateTriangles();
+        MeshUpdate();
     }
 
 
